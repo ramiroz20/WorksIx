889aea93f27e205c382447f561669a6ae707aa88
Roman <66552401+ramiroz20@users.noreply.github.com>
2020-07-02 19:36:17 +0300

    Add files via upload

---
+++ WorkSiix/src/com/company/Worksix.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Worksix {
+
+    public static boolean main(String[] args) {
+        // public boolean isALessThanB ( int a, int b){
+        int b = 2;
+        {
+            boolean b1 = (b > 0);
+            if (b1)
+                return false;
+            else
+                return true;
+        }
+    }
+}
+
+

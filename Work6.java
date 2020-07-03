00dceab543ba98a1dd80a403eb75f56720fa4e70
Roman <66552401+ramiroz20@users.noreply.github.com>
2020-07-03 16:11:55 +0300

    Update Work6.java

--- Work6.java
+++ Work6.java
@@ -3,16 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int b = 2;
-        int a = 1;
-        boolean m = (a > b);
-        if (m == true)
-            System.out.println(true);
-        if (a < b)
-            System.out.println(false);
-        
-    }
-    
-}
 
+
+        boolean m = false;
+        boolean bool = true;
+        if (bool) { // если bool == true
+            System.out.println(m); // ...
+
+            if (!bool) { // если bool == false
+
+            }
+
+        }
+
+    }
+}
 

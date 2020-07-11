dd29c203186aa6d22ed2d3c57fdf3d595985ffeb
Roman <66552401+ramiroz20@users.noreply.github.com>
2020-07-11 20:34:00 +0300

    Update Worksix.java
    Упростил

--- WorkSiix/src/com/company/Worksix.java
+++ WorkSiix/src/com/company/Worksix.java
@@ -1,18 +1,11 @@
 package com.company;
 
 public class Worksix {
-
-    public static boolean main(String[] args) {
-        // public boolean isALessThanB ( int a, int b){
-        int b = 2;
-        {
-            boolean b1 = (b > 0);
-            if (b1)
-                return false;
-            else
-                return true;
-        }
-    }
+	int a=3;									 	int b= 4;
+boolean m = a >b;
+System.out.println(m)
+    
 }
 
 
+}
